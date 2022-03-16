04333e4c965b0e41dbb8f90713b54c2dda95beb3
Arya Moghaddam <ajavadim@uwaterloo.ca>
2022-03-15 22:50:40 -0400

    Update Rotate Array.py

--- Interview-Questions/Rotate Array.py
+++ Interview-Questions/Rotate Array.py
@@ -3,8 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #nums = [1,2,3,4,5,6,7], k = 3
+        #Desired Answer: [5,6,7,1,2,3,4]
+        #Answer revered: [4,3,2,1,7,6,5]
+        #reveresed: [7,6,5,4,3,2,1]
+        #add: [7,6,5,4,3,2,1,7,6,5] (i in range 3)
+        #reverse the  result
         nums.reverse()
-        for i in range(k):
+        for i in range(k): # goes in a loop of k-1
             nums.append(nums[i])
         nums[:] = nums[k:]
         nums.reverse()
